b64a795db6c3361a464c0cb8ab8e167754b1b901
Jessy Trejo <keine.9010@gmail.com>
2022-11-27 14:20:38 -0600

    Corrected URL images on js controllers

--- js/controllers/admin-itemList-controller.js
+++ js/controllers/admin-itemList-controller.js
@@ -9,7 +9,7 @@ const createItemList = (name,price,url,id,categoryId) => {
         <img src="../assets/icons/delete1.svg" alt="Borrar" class="category__admin-img btn-delete" data-delete>
         <a href="admin-item__edit-form.html?id=${id}" data-link><img src="../assets/icons/edit1.svg" alt="Editar" class="category__admin-img btn-edit" data-edit></a>
     </div>
-    <img src="../${url}" alt="Imagen del producto" class="category__box-img">
+    <img src="${url}" alt="Imagen del producto" class="category__box-img">
     <ul class="category__box-body">
         <li class="category__box-text">${name}</li>
         <li class="category__box-text"><p class="category__box__item-price" data-item="price">$${price}.00</p></li>
@@ -20,11 +20,14 @@ const createItemList = (name,price,url,id,categoryId) => {
     // Método para eliminar productos desde el ícono
     const deleteIcon = li.querySelector('[data-delete]');
     deleteIcon.addEventListener("click", () => {
-        if (window.confirm("¿Realmente quiere eliminar este producto?")) {
+        //if (window.confirm("¿Realmente quiere eliminar este producto?"))
+        const passDelete = (window.prompt("Ingrese la contraseña para confirmar la eliminación"));
+        if(passDelete === "trejo9010") {
             itemServices.deleteItem(id).then( () => {
                 console.log(`Producto con ID #${id} eliminado`);
             }).catch((error) => console.log("Ocurrió un error: " + error));
         }
+        else (alert("Disculpa, no me borres mi base de datos :3"));
     });
 
     return li
